ee0979bc45ff1c621e419022ab65ea27d1602c82
Alex Sadler <rookwood101@users.noreply.github.com>
2023-07-30 22:15:26 +0100

    Add mobs

---
+++ mob.gd
@@ -0,0 +1,20 @@
+extends CharacterBody3D
+
+@export var min_speed = 10
+@export var max_speed = 18
+
+# This function will be called from the Main scene
+func initialize(start_position, player_position):
+	look_at_from_position(start_position, player_position, Vector3.UP)
+	rotate_y(randf_range(-PI/8, PI/8))
+	
+	var random_speed = randi_range(min_speed, max_speed)
+	velocity = Vector3.FORWARD * random_speed
+	velocity.rotated(Vector3.UP, rotation.y)
+
+func _physics_process(delta):
+	move_and_slide()
+
+
+func _on_screen_exited():
+	queue_free()
